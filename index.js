b0bb2f1b3c815c85cf64fee1561668cfb247c7ba
Szász Tibor <tszasz@gmail.com>
2017-05-13 22:17:19 +0200

    feat(main): sequence runs smoothly till glitcing

--- index.js
+++ index.js
@@ -6,31 +6,17 @@
  * (c) 2017, Budapest
  */
 
-const gifwriter = require("writegif");
-const TwitterApp = require("./twitter-client");
 const Giphy = require("./giphy");
+const Glitcher = require('./glitch');
+const TwitterApp = require("./twitter-client");
 
 /*TwitterApp.addGif('img/car.gif');
 TwitterApp.setMessage('This is my neat message.');
 */
 /*TwitterApp.sendTweet();*/
 
-Giphy.grab().then(function (){
-});
-
-TwitterApp.onComplete = function() {
-
-}
-
-/*
-*/
-
-function complete() {
-	const orig = fs.readFileSync('download.gif')
-
-	glitch(orig, function (err, img) {
-		gifwriter(img, function (err, gif) {
-			fs.writeFileSync("glitch.gif", gif)
-		});
+Giphy.grab()
+	.then(Glitcher)
+	.then(function() {
+		console.log('GLICHED')
 	});
-}
