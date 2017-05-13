04d1bc06d12947b6f180101339ce39f873d9ddc4
Szász Tibor <tszasz@gmail.com>
2017-05-13 22:18:45 +0200

    feat(glitcher): formatted, fixed displacer fx

--- glitches/displacer.js
+++ glitches/displacer.js
@@ -1,16 +1,16 @@
-var glitcher = require("glitcher")
-var readimage = require("readimage")
+const readimage = require('readimage');
+const glitcher = require('glitcher');
 
 module.exports = function glitch(orig, callback) {
-  readimage(orig, function (err, image) {
-    if (err) {
-      return callback(err)
-    }
-    image.frames.forEach(function (frame) {
-      glitcher.pixelshift(frame.data, Math.floor(Math.random()*15))
-      glitcher.reverseRGBA(frame.data)
-      glitcher.redBlueOverlay(frame.data)
-    })
-    return callback(null, image)
-  })
+	readimage(orig, function (err, image) {
+		if (err) {
+			return callback(err);
+		}
+		image.frames.forEach(function (frame) {
+			glitcher.pixelshift(frame.data, Math.floor(Math.random()*15));
+			glitcher.reverseRGBA(frame.data);
+			glitcher.redBlueOverlay(frame.data);
+		})
+		return callback(null, image);
+	});
 }
